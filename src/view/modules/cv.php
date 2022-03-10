45f66f616f412539f09d61f3ac579924208906e3
florian813 <56045484+florian813@users.noreply.github.com>
2022-03-10 23:47:05 +0100

    Add files via upload

--- src/view/modules/cv.php
+++ src/view/modules/cv.php
@@ -4,8 +4,7 @@ div.text-photo
 	margin-left:13.3%;
 	margin-top:-6%;
 	text-transform: uppercase;
-	letter-spacing: 0.15em;
-	width:12em;
+	width:auto;
 }
 
 hr{width:device-width
@@ -28,7 +27,7 @@ hr{width:device-width
 			Emploi saisonnier<br>
 		</div>
 
-		<div style="margin-left:40% ; margin-top:-2.6% ;">
+		<div style="margin-left:38% ; margin-top:-2.6% ; background:green ;width:30%; text-transform: uppercase;text:100% ">
 			<svg xmlns="http://www.w3.org/2000/svg" width="2%" height="2%" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
   				<path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
 			</svg>
