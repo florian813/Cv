7da49db3819533c65041031b86cdcce37d9667cd
florian813 <56045484+florian813@users.noreply.github.com>
2022-03-10 22:25:09 +0100

    Add files via upload

--- src/view/commons/head.php
+++ src/view/commons/head.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="user-scalable=no,width=device-width" />
 
 <link rel="stylesheet" href="/public/styles/default.css" />
 <link rel="icon" type="image/png" sizes="16x16" href="/public/images/avatar.png">
