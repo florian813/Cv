e4ed2f8a79bac2f0c46a071d7499bfdbad9aebcc
florian813 <56045484+florian813@users.noreply.github.com>
2022-03-11 00:20:51 +0100

    Add files via upload

--- src/view/modules/cv.php
+++ src/view/modules/cv.php
@@ -4,7 +4,8 @@ div.text-photo
 	margin-left:13.3%;
 	margin-top:-6%;
 	text-transform: uppercase;
-	width:auto;
+	width:8%;
+	background:red;
 }
 
 hr{width:device-width
@@ -16,10 +17,10 @@ hr{width:device-width
 
 	<hr>
 
-	<div id="div-photo" style="width=device-width ; background:#273746 ; margin-top:1%">
+	<div id="div-photo" style="width=device-width ; background:#273746 ; margin-top:-0.8%">
 
-		<div style="margin-left:3.4% ; height:5% ; width:9.4%;background:black;">
-			<img src="./img/photo_cv.jpg" class="rounded-circle" style="width: 100% ; margin-top: 1%;"></img>
+		<div style="margin-left:4em ; height:5% ; width:9.4%;background:black;">
+			<img src="./img/photo_cv.jpg" class="rounded-circle" style="width: 100% ; margin-top:2% ;"></img>
 		</div>
 
 		<div class="text-photo">
@@ -27,44 +28,37 @@ hr{width:device-width
 			Emploi saisonnier<br>
 		</div>
 
-		<div style="margin-left: ; margin-top:-6.8% ;width=device-width; text-transform: uppercase;text:100% ">
-			<center>
-			<div>
+		<div style="margin-left:40% ; margin-top:-3% ;">
 			<svg xmlns="http://www.w3.org/2000/svg" width="2%" height="2%" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
   				<path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
 			</svg>
 			florianleborgne813@gmail.com
-			</div>
-			<div>
-			<svg xmlns="http://www.w3.org/2000/svg" width="2%" height="2%" style=";" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+
+			<svg xmlns="http://www.w3.org/2000/svg" width="2%" height="2%" style="margin-left:1%;" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
   				<path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
 			</svg>
 			06 65 97 16 90
-			</div>
-			<div>
+
 			<svg xmlns="http://www.w3.org/2000/svg" style="margin-left:0.9%" width="2%" height="2%" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
   				<path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
 			</svg>
 			07/01/2001<br>
-			</div>
+
 			<svg xmlns="http://www.w3.org/2000/svg" width="2%" height="2%" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
   				<path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
 			</svg>
 			<span>31 rue de la gare ,Beutin 62170</span>
-			<div>
-			<svg xmlns="http://www.w3.org/2000/svg" style="" width="2%" height="2%" fill="currentColor" class="bi bi-credit-card-2-front" viewBox="0 0 16 16">
+
+			<svg xmlns="http://www.w3.org/2000/svg" style="margin-left:1.80%" width="2%" height="2%" fill="currentColor" class="bi bi-credit-card-2-front" viewBox="0 0 16 16">
   				<path d="M14 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2z"/>
   				<path d="M2 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z"/>
 			</svg>
 			<span>Permis B</span>
-			</div>
-			<div>
-			<img src="./img/logo-vehicule.png" style="width: 2% ;"></img>
+	
+			<img src="./img/logo-vehicule.png" style="width: 2% ; margin-left:3.60%"></img>
 			Véhicule personnel
-			</div>
-			<center>
 		</div>
-		<hr style="margin-top:-0.1% ">
+		<hr style="margin-top:4% ">
 	</div>
 
 	
