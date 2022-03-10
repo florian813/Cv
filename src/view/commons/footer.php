426975d13040db73370143565a72a5e151522425
florian813 <56045484+florian813@users.noreply.github.com>
2022-03-10 16:40:44 +0100

    Add files via upload

--- src/view/commons/footer.php
+++ src/view/commons/footer.php
@@ -1,8 +1,7 @@
 
-
-
 <?php
-  echo"<footer>
+  echo"<footer style='border-top-style: none;width:114.95em;'>
+            <hr style='width:115.95em ;'>
             <a href='/apropos'><img width='32' src='/public/images/avatar.png'>À-propos</a>
             <p><strong>Projet Web</strong></p>
             <p>Florian Leborgne</p>
