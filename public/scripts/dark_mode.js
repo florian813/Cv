8ae51217c2cc8cf81e8ff7af6d350060b7b0cb22
florian813 <56045484+florian813@users.noreply.github.com>
2022-03-10 22:02:35 +0100

    Add files via upload

--- public/scripts/dark_mode.js
+++ public/scripts/dark_mode.js
@@ -6,10 +6,12 @@ function darkFunction() {
         body.style.background="#191A19";
         body.style.color="white";
         document.getElementById("light-button").textContent="light mode";
+        document.getElementById("div-photo").style.background="#273746";
     }else{
         body.style.background="white";
         body.style.color="#191A19";
         document.getElementById("light-button").textContent="dark mode";
+        document.getElementById("div-photo").style.background="#7FB3D5";
     }
 }
 </script>
