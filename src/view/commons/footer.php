399e1f0d5ca3d626f54cdfdc9dbfb6419c980d69
florian813 <56045484+florian813@users.noreply.github.com>
2022-03-14 20:46:14 +0100

    Update footer.php

--- src/view/commons/footer.php
+++ src/view/commons/footer.php
@@ -2,8 +2,9 @@
 <?php
   echo"<footer style='border-top-style: none;width=device-width'>
             <hr style='width=device-width'>
-            <a href='/apropos'><img width='32' src='/public/images/avatar.png'>À-propos</a>
-            <p><strong>CV</strong></p>
+            
             <p>Florian Leborgne</p>
+            <p><strong>CV</strong></p>
+            <a href='/apropos'><img width='32' src='/public/images/avatar.png'>À-propos</a>
         </footer>";
 ?>
