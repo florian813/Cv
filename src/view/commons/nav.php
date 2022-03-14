0985ec009cdcb736da74fe60ffe36a7156c18d5b
florian813 <56045484+florian813@users.noreply.github.com>
2022-03-14 23:41:37 +0100

    Update nav.php

--- src/view/commons/nav.php
+++ src/view/commons/nav.php
@@ -1,15 +1,10 @@
-<div>
     <nav style="width=device-width">
         
         <img src="/public/images/avatar.png" class="navbar-brand" href="#" style="width: 2%;height: 2%; margin-left: 1%;"></img>
       
-        <div>
    
               <a href="/">Cv</a>
         
           <button onclick="darkFunction()" id="light-button" style="margin-left:81%;">dark mode</button>
-        </div>
             
       </nav>
-    
-</div>
