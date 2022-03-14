3e116a9079fade276706a23920d6c7c52501f169
florian813 <56045484+florian813@users.noreply.github.com>
2022-03-15 00:06:56 +0100

    Update dark_mode.js

--- public/scripts/dark_mode.js
+++ public/scripts/dark_mode.js
@@ -6,13 +6,13 @@ function darkFunction() {
         body.style.background="#191A19";
         body.style.color="white";
         document.getElementById("light-button").textContent="light mode";
-        document.getElementById("light-button").class="btn btn-light";
+        document.getElementById("light-button").className="btn btn-light";
         document.getElementById("div-photo").style.background="#273746";
     }else{
         body.style.background="white";
         body.style.color="#191A19";
         document.getElementById("light-button").textContent="dark mode";
-        document.getElementById("light-button").class="btn btn-dark";
+        document.getElementById("light-button").className="btn btn-dark";
         document.getElementById("div-photo").style.background="#7FB3D5";
     }
 }
