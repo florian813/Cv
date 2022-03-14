d8a9a000a888c17d338c309b2daa291a7576a028
florian813 <56045484+florian813@users.noreply.github.com>
2022-03-14 23:29:10 +0100

    Update nav.php

--- src/view/commons/nav.php
+++ src/view/commons/nav.php
@@ -2,9 +2,6 @@
     <nav style="width=device-width">
         
         <img src="/public/images/avatar.png" class="navbar-brand" href="#" style="width: 2%;height: 2%; margin-left: 1%;"></img>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
       
         <div>
    
