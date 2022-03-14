5788ea7ce8e23460464c39b3ae0129e8efafc56a
florian813 <56045484+florian813@users.noreply.github.com>
2022-03-14 20:44:51 +0100

    Update footer.php

--- src/view/commons/footer.php
+++ src/view/commons/footer.php
@@ -5,6 +5,5 @@
             <a href='/apropos'><img width='32' src='/public/images/avatar.png'>À-propos</a>
             <p><strong>CV</strong></p>
             <p>Florian Leborgne</p>
-            <p>L2 Informatique - 2022</p>
         </footer>";
 ?>
