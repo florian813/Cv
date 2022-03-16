d67d62db21c8f6383a4e94ade17136bd60b9a777
florian813 <56045484+florian813@users.noreply.github.com>
2022-03-16 17:58:48 +0100

    Update nav.php

--- src/view/commons/nav.php
+++ src/view/commons/nav.php
@@ -5,6 +5,6 @@
    
               <a href="/">Cv</a>
         
-          <button onclick="darkFunction()" id="light-button" style="margin-left:70%;font-size:85%" type="button" class="btn btn-dark" style="border:2px red">dark mode</button>
+          <button onclick="darkFunction()" id="light-button" style="margin-left:70%;font-size:85%" type="button" class="btn btn-dark" style="border-style:groove">dark mode</button>
             
       </nav>
