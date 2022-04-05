974f2d5b80c067dbedc26431d30e86e9215d0961
florian813 <56045484+florian813@users.noreply.github.com>
2022-04-05 17:56:28 +0200

    Add files via upload

--- public/scripts/dark_mode.js
+++ public/scripts/dark_mode.js
@@ -8,12 +8,14 @@ function darkFunction() {
         document.getElementById("light-button").textContent="light mode";
         document.getElementById("light-button").className="btn btn-light";
         document.getElementById("div-photo").style.background="#273746";
+        document.getElementById("nav").style.background="#191A19";
     }else{
         body.style.background="white";
         body.style.color="#191A19";
         document.getElementById("light-button").textContent="dark mode";
         document.getElementById("light-button").className="btn btn-dark";
         document.getElementById("div-photo").style.background="#7FB3D5";
+        document.getElementById("nav").style.background="white";
     }
 }
 </script>
--- src/view/commons/nav.php
+++ src/view/commons/nav.php
@@ -1,9 +1,9 @@
-    <nav style="width=device-width">
+    <nav id="nav" style="width=device-width ;background-color:#191A19">
         
         <img src="img/cv.png" class="navbar-brand" href="#" style="width: 2%;height: 2%; margin-left: 1%;"></img>
       
    
-        <h1 style="margin-left:1% c;color:black">CV</h1>
+        <h1 style="margin-left:1% ;">CV</h1>
         
           <button onclick="darkFunction()" id="light-button" style="margin-left:70%;font-size:85%" type="button" class="btn btn-dark" style="border-style:double;border-width: Medium">dark mode</button>
             
