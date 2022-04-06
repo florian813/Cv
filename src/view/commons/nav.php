cd31f19d4dc015731a9090b38d191cec4462700f
florian813 <56045484+florian813@users.noreply.github.com>
2022-04-07 01:15:54 +0200

    Update nav.php

--- src/view/commons/nav.php
+++ src/view/commons/nav.php
@@ -1,8 +1,7 @@
     <nav id="nav" style="width=device-width ;background-color:#191A19;">
 
         <h1 style="margin-left:1% ;">CV</h1>
-            <a href="#diplomes">Diplomes et formations</a>
-        
+                 
           <button onclick="darkFunction()" id="light-button" style="margin-left:60%;font-size:85%" type="button" class="btn btn-dark" style="border-style:double;border-width: Medium">dark mode</button>
             
       </nav>
